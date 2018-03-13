7f9eda740726734b0c65232794b1fb10202e7d3d
pvbl <paulvanlorenzo@gmail.com>
2018-03-13 19:25:01 +0100

    añadido opcion de coger un limite de telefonos

--- webhook.py
+++ webhook.py
@@ -32,30 +32,35 @@ def lambda_function(req,context):
     return res
     
 
-def searchElCorteIngles(product,limit = None):
+def searchElCorteIngles(product,limit = -1,inbound=0):
     website = 'https://www.elcorteingles.es/'
-    query = website + 'search/?s=' + product
+    query = website + 'search/?s=telefono+' + product
     r = requests.get(query)
     soup = BeautifulSoup(r.content,"html5lib")
     items = soup.find("ul",{"class":"product-list 4"}).find_all("li")
-    speech = "Los {0} Productos mas vendidos son:".format(str(limit) if limit else "")
+    #speech = "Los {0} Productos mas vendidos son:".format(str(limit) if limit else
+    speech= '' 
     for i, item in enumerate(items):
+        if (i<inbound):
+            continue
         datajson = json.loads(item.find("span")['data-json'])
         name = datajson["name"]
-        price = datajson["price"]['final'] if "final" in datajson["price"] else "desconodido"
-        speech = speech + "{0}: {1} a {2} euros.\n ".format(i,name ,price)
-        if (limit!=None) & (i == limit):
+        price = 'a {0} Euros.'.format( datajson["price"]['final']) if "final" in datajson["price"] else ""
+        speech = speech + "{0}: {1} {2}.\n ".format(i,name ,price)
+        if (i == limit):
             break
     return  { "speech": speech,
               "displayText": speech,
-              "source": "apiai-weather-webhook"
+              "source": "apiai-eci"
             }
 
 def makeResponse(req,parameter='item'):      
     result = req.get("result")
     parameters = result.get("parameters")
     item = parameters.get(parameter)
-    return searchElCorteIngles(item)
+    inbound=parameters.get("inbound") if parameters.get("inbound") else 0
+    limit=parameters.get("limit") if parameters.get("limit") else -1
+    return searchElCorteIngles(item, limit, inbound)
 
 
 
